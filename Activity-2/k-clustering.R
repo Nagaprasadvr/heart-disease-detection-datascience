7e2e15192cb39839a155056f866500081c79896c
Nagaprasadvr@#£ <nagaprasadvr246@gmail.com>
2022-01-15 10:11:53 +0530

    k-means completed

---
+++ Activity-2/k-clustering.R
@@ -0,0 +1,19 @@
+library(factoextra)
+library(readr)
+#load data
+heart <- read_csv("Activity-2/heart.csv")
+#scale data
+heart_scale <- scale(heart[1:13])
+#View(heart_scale)
+#distance
+heart_dist <- dist(heart_scale)
+#heart_dist
+#calculate number of clusters
+(fviz_nbclust(heart_scale, kmeans , method = "wss" ) + 
+  labs(subtitle = "Elbow method"))
+km.out <- kmeans(hear_scale,centers = 5 ,nstart = 100)
+print(km.out)
+#results
+km.clusters <- km.out$cluster
+fig <- fviz_cluster(list(data = heart_scale, cluster = km.clusters))
+plot(fig)
