797c2b1a1cdc84833293709880770623528373c1
Nagaprasadvr@#£ <nagaprasadvr246@gmail.com>
2022-01-15 20:23:41 +0530

    h-clustering

---
+++ Activity-2/h-clustering.R
@@ -0,0 +1,13 @@
+library(factoextra)
+#load data
+library(readr)
+heart <- read_csv("Activity-2/heart.csv")
+#scale data
+heart_scale <- scale(heart[1:13])
+#View(heart_scale)
+#distance
+heart_dist <- dist(heart_scale)
+#View(heart_dist)
+#hierarchical clustering algorithm
+hc.out <- hclust(heart_dist, method = "complete")
+plot(hc.out)
